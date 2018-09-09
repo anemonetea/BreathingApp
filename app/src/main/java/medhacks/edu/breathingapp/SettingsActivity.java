e55afa91e2ccb3a27d765ff6c671531de6b95e99
bzc20 <bethany.zc@gmail.com>
2018-09-09 03:51:53 -0400

    edited settings

--- app/src/main/java/medhacks/edu/breathingapp/SettingsActivity.java
+++ app/src/main/java/medhacks/edu/breathingapp/SettingsActivity.java
@@ -3,6 +3,11 @@ package medhacks.edu.breathingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+//min score - goal?
+//max score - general max, don't go above 3k
+//personal max?
+
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
